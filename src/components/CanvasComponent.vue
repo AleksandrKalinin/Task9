389d7c3cf47d0086235787437470f5bece021659
AleksandrKalinin <aleksandr_kalinin_1995@mail.ru>
2023-02-07 16:54:21 +0300

    Implemented main canvas methods: drawing shapes, lines etc.

--- src/components/CanvasComponent.vue
+++ src/components/CanvasComponent.vue
@@ -16,7 +16,6 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "CanvasComponent",
-
   data() {
     return {
       canvas: null as any,
@@ -36,18 +35,6 @@ export default defineComponent({
   },
 
   methods: {
-    draw(e: any) {
-      console.log(e);
-    },
-
-    checkIfShapeSelected(e: any) {
-      console.log(e);
-    },
-
-    checkIfShapeActive(e: any) {
-      console.log(e);
-    },
-
     initializeCanvas() {
       const myCanvas: any = this.$refs.myCanvas;
       this.canvas = myCanvas;
@@ -64,6 +51,220 @@ export default defineComponent({
       const canvas: HTMLCanvasElement = this.canvas;
       const imgPath: any = canvas.toDataURL("image/png");
       this.saved.push(imgPath);
+    },
+
+    draw(e: MouseEvent) {
+      if (this.isDrawing) {
+        this.drawOnCanvas(this.x, this.y, e.offsetX, e.offsetY);
+        this.x = e.offsetX;
+        this.y = e.offsetY;
+      }
+    },
+
+    startDrawing(e: any) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
+      this.isDrawing = true;
+    },
+
+    stopDrawing(e: any) {
+      if (this.isDrawing) {
+        this.drawOnCanvas(this.x, this.y, e.offsetX, e.offsetY);
+        this.x = 0;
+        this.y = 0;
+        this.isDrawing = false;
+      }
+    },
+
+    drawOnCanvas(x1: any, y1: any, x2: any, y2: any) {
+      const ctx = this.canvas.getContext("2d");
+      ctx.beginPath();
+      ctx.strokeStyle = this.color;
+      ctx.lineWidth = this.lineWidth;
+      ctx.moveTo(x1, y1);
+      ctx.lineTo(x2, y2);
+      ctx.stroke();
+      ctx.closePath();
+    },
+
+    getStartCoords(e: any) {
+      const cX = e.target.getBoundingClientRect().left + window.scrollX;
+      const cY = e.target.getBoundingClientRect().top + window.scrollY;
+      this.startX = e.pageX - cX;
+      this.startY = e.pageY - cY;
+    },
+
+    getEndCoords(e: any) {
+      const cX = e.target.getBoundingClientRect().left + window.scrollX;
+      const cY = e.target.getBoundingClientRect().top + window.scrollY;
+      this.endX = e.pageX - cX;
+      this.endY = e.pageY - cY;
+      if (this.startX !== this.endX) {
+        if (this.startY > this.endY) {
+          let tempX = this.startX;
+          let tempY = this.startY;
+          this.startX = this.endX;
+          this.startY = this.endY;
+          this.endX = tempX;
+          this.endY = tempY;
+        }
+        this.drawShape(e);
+      }
+    },
+
+    drawEllipse(e: any) {
+      const canvas = this.canvas;
+      const ctx = canvas.getContext("2d");
+      ctx.beginPath();
+      ctx.fillStyle = this.color;
+      ctx.strokeStyle = this.color;
+      ctx.lineWidth = this.lineWidth;
+      let mainAxis = Math.sqrt(
+        Math.pow(this.endX - this.startX, 2) -
+          Math.pow(this.endY - this.startY, 2)
+      );
+      let angle = Math.atan(
+        (this.endY - this.startY) / (this.endX - this.startX)
+      );
+      ctx.ellipse(
+        this.startX,
+        this.startY,
+        mainAxis / 2,
+        mainAxis / 4,
+        angle,
+        0,
+        2 * Math.PI
+      );
+      ctx.stroke();
+      ctx.closePath();
+      this.canvas = canvas;
+    },
+
+    drawStar(e: any) {
+      const canvas = this.canvas;
+      const ctx = canvas.getContext("2d");
+      ctx.fillStyle = this.color;
+      ctx.strokeStyle = this.color;
+      ctx.lineWidth = this.lineWidth;
+      var rot = (Math.PI / 2) * 3;
+      var x = this.startX;
+      var y = this.startY;
+      var step = Math.PI / 5;
+      let outerRadius = Math.sqrt(
+        Math.pow(this.endX - this.startX, 2) -
+          Math.pow(this.endY - this.startY, 2)
+      );
+      let innerRadius = outerRadius / 2;
+      ctx.beginPath();
+      ctx.moveTo(this.startX, this.startY - outerRadius);
+      for (var i = 0; i < 5; i++) {
+        x = this.startX + Math.cos(rot) * outerRadius;
+        y = this.startY + Math.sin(rot) * outerRadius;
+        ctx.lineTo(x, y);
+        rot += step;
+        x = this.startX + Math.cos(rot) * innerRadius;
+        y = this.startY + Math.sin(rot) * innerRadius;
+        ctx.lineTo(x, y);
+        rot += step;
+      }
+      ctx.lineTo(this.startX, this.startY - outerRadius);
+      ctx.closePath();
+      ctx.lineWidth = 5;
+      ctx.strokeStyle = "blue";
+      ctx.stroke();
+      ctx.fillStyle = "skyblue";
+      ctx.fill();
+      ctx.stroke();
+      ctx.closePath();
+      this.canvas = canvas;
+    },
+
+    drawRectangle(e: any) {
+      const canvas = this.canvas;
+      const ctx = canvas.getContext("2d");
+      ctx.beginPath();
+      ctx.fillStyle = this.color;
+      ctx.strokeStyle = this.color;
+      ctx.lineWidth = this.lineWidth;
+      ctx.moveTo(this.startX, this.startY);
+      ctx.lineTo(this.startX, this.endY);
+      ctx.lineTo(this.endX, this.endY);
+      ctx.lineTo(this.endX, this.startY);
+      ctx.lineTo(this.startX, this.startY);
+      ctx.stroke();
+      ctx.closePath();
+      ctx.closePath();
+      this.canvas = canvas;
+    },
+
+    drawCircle(e: any) {
+      const canvas = this.canvas;
+      const ctx = canvas.getContext("2d");
+      ctx.beginPath();
+      ctx.fillStyle = this.color;
+      ctx.strokeStyle = this.color;
+      ctx.lineWidth = this.lineWidth;
+      let diameter = Math.sqrt(
+        Math.pow(this.endX - this.startX, 2) -
+          Math.pow(this.endY - this.startY, 2)
+      );
+      ctx.arc(this.startX, this.startY, diameter, 0, 2 * Math.PI);
+      ctx.stroke();
+      ctx.closePath();
+      this.canvas = canvas;
+    },
+
+    drawTriangle(e: any) {
+      const canvas = this.canvas;
+      const ctx = canvas.getContext("2d");
+      ctx.beginPath();
+      ctx.fillStyle = this.color;
+      ctx.strokeStyle = this.color;
+      ctx.lineWidth = this.lineWidth;
+      ctx.moveTo(this.startX + (this.endX - this.startX) / 2, this.startY);
+      ctx.lineTo(this.startX, this.endY);
+      ctx.lineTo(this.endX, this.endY);
+      ctx.lineTo(this.startX + (this.endX - this.startX) / 2, this.startY);
+      ctx.stroke();
+      ctx.closePath();
+      this.canvas = canvas;
+    },
+
+    checkIfShapeSelected(e: MouseEvent) {
+      console.log(this.selectedShape);
+      if (this.selectedShape === null) {
+        this.startDrawing(e);
+      } else {
+        this.getStartCoords(e);
+      }
+    },
+
+    checkIfShapeActive(e: MouseEvent) {
+      if (this.selectedShape === null) {
+        this.stopDrawing(e);
+      } else {
+        this.getEndCoords(e);
+      }
+    },
+
+    drawShape(e: any) {
+      if (this.selectedShape === "triangle") {
+        this.drawTriangle(e);
+      } else if (this.selectedShape === "circle") {
+        this.drawCircle(e);
+      } else if (this.selectedShape === "rectangle") {
+        this.drawRectangle(e);
+      } else if (this.selectedShape === "ellipse") {
+        this.drawEllipse(e);
+      } else if (this.selectedShape === "octagon") {
+        //this.drawOctagon(e);
+      } else if (this.selectedShape === "hexagon") {
+        //this.drawHexagon(e);
+      } else if (this.selectedShape === "star") {
+        this.drawStar(e);
+      } else if (this.selectedShape === "square") {
+        //this.drawSquare(e);
+      }
     },
   },
 
--- src/components/CanvasMenu.vue
+++ src/components/CanvasMenu.vue
@@ -1,5 +1,77 @@
 <template>
-  <div class="canvas-menu"></div>
+  <div class="canvas-menu">
+    <div class="canvas-menu__section">
+      <h6 class="canvas-menu__category">Instruments</h6>
+      <div class="canvas-menu__wrapper">
+        <div class="canvas-menu__item">
+          <img
+            class="canvas-menu__icon"
+            v-bind:src="require('@/assets/paintbrush-solid.svg')"
+          />
+        </div>
+      </div>
+      <h6 class="canvas-menu__category">Colors</h6>
+      <div class="canvas-menu__wrapper">
+        <div
+          class="canvas-menu__item"
+          v-bind:key="index"
+          v-for="(item, index) in colors"
+          :style="{ 'background-color': item }"
+        ></div>
+      </div>
+      <h6 class="canvas-menu__category">Shapes</h6>
+      <div class="canvas-menu__wrapper">
+        <div class="canvas-menu__item" @click="selectShape('triangle')">
+          <img
+            class="canvas-menu__icon"
+            v-bind:src="require('@/assets/triangle-solid.svg')"
+          />
+        </div>
+        <div class="canvas-menu__item" @click="selectShape('circle')">
+          <img
+            class="canvas-menu__icon"
+            v-bind:src="require('@/assets/circle-regular.svg')"
+          />
+        </div>
+        <div class="canvas-menu__item" @click="selectShape('ellipse')">
+          <img
+            class="canvas-menu__icon"
+            v-bind:src="require('@/assets/ellipse.svg')"
+          />
+        </div>
+        <div class="canvas-menu__item" @click="selectShape('rectangle')">
+          <img
+            class="canvas-menu__icon"
+            v-bind:src="require('@/assets/rectangle.svg')"
+          />
+        </div>
+        <div class="canvas-menu__item" @click="selectShape('square')">
+          <img
+            class="canvas-menu__icon"
+            v-bind:src="require('@/assets/square-regular.svg')"
+          />
+        </div>
+        <div class="canvas-menu__item" @click="selectShape('star')">
+          <img
+            class="canvas-menu__icon"
+            v-bind:src="require('@/assets/star.svg')"
+          />
+        </div>
+        <div class="canvas-menu__item" @click="selectShape('octagon')">
+          <img
+            class="canvas-menu__icon"
+            v-bind:src="require('@/assets/octagon.svg')"
+          />
+        </div>
+        <div class="canvas-menu__item" @click="selectShape('hexagon')">
+          <img
+            class="canvas-menu__icon"
+            v-bind:src="require('@/assets/hexagon.svg')"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,14 +79,70 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "CanvasMenu",
+
+  data() {
+    return {
+      colors: [
+        "#000000",
+        "#FFFFFF",
+        "#037949",
+        "#318CE7",
+        "#993300",
+        "#CC338B",
+        "#BFFF00",
+        "#D40000",
+        "#000000",
+        "#FFFFFF",
+        "#037949",
+        "#318CE7",
+        "#993300",
+        "#CC338B",
+        "#BFFF00",
+        "#D40000",
+      ],
+    };
+  },
+
+  methods: {
+    selectShape(value) {
+      this.selectedShape = value;
+    },
+  },
 });
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="sass">
 .canvas-menu
-  width: 300px
+  width: 445px
   height: 500px
   border: 1px solid #CCCCCC
   margin-right: 30px
+
+  .canvas-menu__category
+    font-size: 18px
+    text-transform: uppercase
+    letter-spacing: 1px
+    padding: 10px 5px
+  .canvas-menu__wrapper
+    display: flex
+    justify-content: space-around
+    flex-wrap: wrap
+  .canvas-menu__item
+    width: 50px
+    height: 50px
+    cursor: pointer
+    display: flex
+    justify-content: center
+    align-items: center
+    margin-bottom: 5px
+    overflow: hidden
+    border: 1px solid #333333
+
+  .canvas-menu__icon
+    width: 30px
+    height: 30px
+    object-fit: cover
+    -webkit-filter: invert(100%)
+    filter: invert(100%)
 </style>
